03ca630459a54f98efdad0848d63fab58a270932
JustFun <paw93@ukr.net>
2026-02-02 20:44:54 +0200

    finished ALL tasks

--- js/task-5.js
+++ js/task-5.js
@@ -3,3 +3,12 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+function changeBackgroundColor() {
+
+const body = document.querySelector('body');
+const color  = getRandomHexColor();
+body.style.backgroundColor = color;
+document.querySelector('.color').textContent = color;
+}
+document.querySelector('.change-color').onclick = changeBackgroundColor;
